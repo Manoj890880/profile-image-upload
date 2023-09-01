44ad6f86962594ade1da514e443f4edfbba25dc8
manoj <manojsahoo890880@gmail.com>
2023-09-01 13:24:24 +0530

    done

--- src/test/java/com/masai/StudentApp4ApplicationTests.java
+++ src/test/java/com/masai/StudentApp4ApplicationTests.java
@@ -1,13 +1,32 @@
 package com.masai;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.hamcrest.Matchers.hasSize;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class StudentApp4ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private MockMvc mockMvc;
 
+	@Test
+	void testGetAllStudents() throws Exception {
+		// Perform a GET request to the "/students/all" endpoint
+		mockMvc.perform(MockMvcRequestBuilders
+						.get("/students/all")
+						.contentType(MediaType.APPLICATION_JSON)
+				)
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
+				.andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(16))); // Update the expected size based on your data
+	}
 }
